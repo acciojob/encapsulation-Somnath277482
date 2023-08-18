38b506b7664b06b3fe72a194f7ddc017bf718843
somnath277482 <som277482@gmail.com>
2023-08-18 22:21:38 +0530

    Trying

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+    RWOnly myObj = new RWOnly();
+    myObj.setName("Somnath");
+    System.out.println(myObj.getName()) ;
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,16 @@
 package com.driver;
+import java.util.* ;
 
 public class RWOnly {
-    
+    private String name ;
+
+    public String getName(){
+        return name ; //getter
+    }
+
+    public void setName(String name){
+        this.name = name ;
+    }
+
+
 }
